97482c9a6a3cbd48dc525a6dfb4e9a338043a151
gustavodezan <63751057+gustavodezan@users.noreply.github.com>
2021-10-01 00:34:28 -0300

    Create frases.py

---
+++ frases.py
@@ -0,0 +1,23 @@
+motivacionais = ["Destino não é exterior a nós; somos nós que criamos nosso próprio destino dia após dia",
+                 "O homem que removeu a montanha começou carregando as pedras",
+                 "O gosto amargo da baixa qualidade dura bem mais do que a doçura do preço inferior",
+                 "A covardia é o medo aceito; a coragem é o medo dominado",
+                 "Todos os dias me levanto para vencer",
+                 "Meu sucesso se deve ao fato de me cercar de pessoas competentes",
+                 "Não atrase o seu progresso com medo de fracassar",
+                 "As pessoas são a força direcional por trás do crescimento","Quem não tem inteligência para criar tem que ter coragem para copiar",
+                 "Ninguém pode ferir você sem o seu consentimento",
+                 "O verdadeiro risco é não fazer nada","Aquele que domina os outros é forte; aquele que domina a si mesmo é todo-poderoso",
+                 "Faça o que puder, com o que tiver, onde estiver",
+                 "A qualidade do serviço e produto é decorrência da qualidade das pessoas",
+                 "Nossas dúvidas são traidoras e nos fazem perder, por medo de tentar, o que poderíamos ganhar",
+                 "Os problemas são apenas oportunidades com roupas de trabalho",
+                 '''⣿⣿⠇⠄⠄⠄⠄⠄⠄⠄⢀⡀⠄⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿
+                ⣿⣿⠄⠄⠄⠄⠄⠈⠉⠁⠄⠉⠹⣦⠟⣿⣿⣿⣿⣿⣿⣿⣿ 
+                ⣿⣿⡄⠄⠄⠄⠄⠄⣀⠐⢄⢀⢸⣀⢀⡿⠟⠋⠉⠙⠻⣿⣿ 
+                ⣿⣿⣿⡄⠄⠄⠄⠄⣧⠠⣄⣩⣤⣤⣥⣤⣴⣿⣶⣤⠄⠘⣿ 
+                ⣿⣿⣿⣿⣷⡀⠄⠄⠈⡉⠦⣬⣉⣉⠙⣿⣿⢿⢿⣿⠄⠄⣿ 
+                ⣿⣿⡿⠿⠿⠇⠄⠄⠄⣿⣷⣦⠹⣿⣤⠄⣠⣦⡀⢃⣠⣾⣿ 
+                ⣿⠟⠁⠄⠄⠄⠄⠄⠄⢠⣿⣿⣿⣷⣤⣤⣾
+                ''']
+# algumas das frases - não são de minha autoria
